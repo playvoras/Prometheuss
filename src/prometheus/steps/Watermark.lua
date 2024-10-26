759d8e412e251d8589abf408dcae8303ec1de0af
playvora <longliator.dev@gmail.com>
2024-10-27 05:25:36 +0700

    Update Watermark.lua

--- src/prometheus/steps/Watermark.lua
+++ src/prometheus/steps/Watermark.lua
@@ -1,61 +1,54 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
+-- This Script is Part of the ObscuraLua Obfuscator by User319183
 --
 -- Watermark.lua
 --
 -- This Script provides a Step that will add a watermark to the script
 
-local Step = require("prometheus.step");
-local Ast = require("prometheus.ast");
-local Scope = require("prometheus.scope");
+local Step = require("prometheus.step")
+local Ast = require("prometheus.ast")
+local Scope = require("prometheus.scope")
+local escape = require("prometheus.util").escape
 
-local Watermark = Step:extend();
-Watermark.Description = "This Step will add a watermark to the script";
-Watermark.Name = "Watermark";
+local Watermark = Step:extend()
+Watermark.Description = "This Step will add a robust watermark to the script"
+Watermark.Name = "RobustWatermark"
 
 Watermark.SettingsDescriptor = {
   Content = {
     name = "Content",
     description = "The Content of the Watermark",
     type = "string",
-    default = "This Script is Part of the Prometheus Obfuscator by Levno_710",
+    default = "PROTECTED"
   },
   CustomVariable = {
     name = "Custom Variable",
     description = "The Variable that will be used for the Watermark",
     type = "string",
-    default = "_WATERMARK",
+    default = "__PROTECTED__",
   }
 }
 
 function Watermark:init(settings)
-	
 end
 
 function Watermark:apply(ast)
-  local body = ast.body;
+  local body = ast.body
   if string.len(self.Content) > 0 then
-    local scope, variable = ast.globalScope:resolve(self.CustomVariable);
-    local watermark = Ast.AssignmentVariable(ast.globalScope, variable);
-
-    local functionScope = Scope:new(body.scope);
-    functionScope:addReferenceToHigherScope(ast.globalScope, variable);
-    
-    local arg = functionScope:addVariable();
-    local statement = Ast.PassSelfFunctionCallStatement(Ast.StringExpression(self.Content), "gsub", {
-      Ast.StringExpression(".+"),
-      Ast.FunctionLiteralExpression({
-        Ast.VariableExpression(functionScope, arg)
-      }, Ast.Block({
-        Ast.AssignmentStatement({
-          watermark
-        }, {
-          Ast.VariableExpression(functionScope, arg)
-        })
-      }, functionScope))
-    });
-
-    table.insert(ast.body.statements, 1, statement)
+    local scope, variable = ast.globalScope:resolve(self.CustomVariable)
+    local watermark = Ast.AssignmentVariable(ast.globalScope, variable)
+    local functionScope = Scope:new(body.scope)
+    functionScope:addReferenceToHigherScope(ast.globalScope, variable)
+    -- Embed the watermark in a less obvious way. Let shouldEscape be false
+    local embeddedWatermark = Ast.WatermarkExpression(self.Content)
+    local statement = Ast.AssignmentStatement({
+      watermark
+    }, {
+      embeddedWatermark
+    })
+    -- Insert the watermark at a random position within the script
+    local insertPosition = math.random(1, #ast.body.statements)
+    table.insert(ast.body.statements, insertPosition, statement)
   end
 end
 
-return Watermark;
+return Watermark
