656a2aa6b66e2f109419f6592a65b893899253c2
playvora <longliator.dev@gmail.com>
2024-10-27 05:24:57 +0700

    Update WatermarkCheck.lua

--- src/prometheus/steps/WatermarkCheck.lua
+++ src/prometheus/steps/WatermarkCheck.lua
@@ -1,50 +1,116 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
+-- This Script is Part of the ObscuraLua Obfuscator by User319183
 -- WatermarkCheck.lua
---
 -- This Script provides a Step that will add a watermark to the script
 
-local Step = require("prometheus.step");
-local Ast = require("prometheus.ast");
-local Scope = require("prometheus.scope");
-local Watermark = require("prometheus.steps.Watermark");
+local Step = require("prometheus.step")
+local Ast = require("prometheus.ast")
+local Scope = require("prometheus.scope")
+local Watermark = require("prometheus.steps.Watermark")
+local EncryptStrings = require("prometheus.steps.EncryptStrings")
+local wrapInFunctionStep = require("prometheus.steps.WrapInFunction")
 
-local WatermarkCheck = Step:extend();
-WatermarkCheck.Description = "This Step will add a watermark to the script";
-WatermarkCheck.Name = "WatermarkCheck";
+local WatermarkCheck = Step:extend()
 
+WatermarkCheck.Description = "This Step will verify the robust watermark in the script"
+WatermarkCheck.Name = "RobustWatermarkCheck"
 WatermarkCheck.SettingsDescriptor = {
   Content = {
     name = "Content",
     description = "The Content of the WatermarkCheck",
     type = "string",
-    default = "This Script is Part of the Prometheus Obfuscator by Levno_710",
+    default =
+    "PROTECTED"
   },
 }
 
-local function callNameGenerator(generatorFunction, ...)
-	if(type(generatorFunction) == "table") then
-		generatorFunction = generatorFunction.generateName;
-	end
-	return generatorFunction(...);
+function WatermarkCheck:init(settings)
+  -- Initialization code here
 end
 
-function WatermarkCheck:init(settings)
+function WatermarkCheck:hash(content)
+  local hash = 5381
+  for i = 1, #content do
+    local char = string.byte(content, i)
+    hash = ((hash * 33) + char) % 0x100000000
+    hash = hash - (hash % (char * 2 ^ ((i % 4) * 8)))
+    hash = hash + (char * 2 ^ ((i % 4) * 8))
+  end
+  return tostring(hash)
+end
 
+function WatermarkCheck:checkRedefinition(ast)
+  for _, statement in ipairs(ast.body.statements) do
+    if statement.type == 'AssignmentStatement' then
+      self:checkVariableInAssignment(statement)
+    elseif statement.type == 'LocalStatement' then
+      self:checkVariableInLocal(statement)
+    elseif statement.type == 'FunctionDeclaration' then
+      self:checkVariableInFunction(statement)
+    end
+  end
+end
+
+function WatermarkCheck:checkVariableInAssignment(statement)
+  for _, variable in ipairs(statement.variables) do
+    if variable.name == self.CustomVariable then
+      self:insertCheck(statement)
+    end
+  end
+end
+
+function WatermarkCheck:checkVariableInLocal(statement)
+  for _, variable in ipairs(statement.variables) do
+    if variable.name == self.CustomVariable then
+      self:insertCheck(statement)
+    end
+  end
+end
+
+function WatermarkCheck:checkVariableInFunction(statement)
+  if statement.identifier and statement.identifier.name == self.CustomVariable then
+    self:insertCheck(statement)
+  end
+end
+
+function WatermarkCheck:insertCheck(statement)
+  local notEqualsExpression = Ast.NotEqualsExpression(Ast.VariableExpression(Ast.globalScope, self.CustomVariable), Ast.StringExpression(self.Content))
+  local ifBody = Ast.Block({ Ast.ReturnStatement({}) }, Scope:new(Ast.body.scope))
+  table.insert(Ast.body.statements, 1, Ast.IfStatement(notEqualsExpression, ifBody, {}, nil))
 end
 
 function WatermarkCheck:apply(ast, pipeline)
-  self.CustomVariable = "_" .. callNameGenerator(pipeline.namegenerator, math.random(10000000000, 100000000000));
-  pipeline:addStep(Watermark:new(self));
+  self:checkRedefinition(ast)
+  -- Keep the custom variable name as __OBSCURALUA__
+  self.CustomVariable = "__PROTECTED__"
+  pipeline:addStep(Watermark:new(self))
+  local body = ast.body
+  local watermarkExpression = Ast.StringExpression(self.Content)
+  local scope, variable = ast.globalScope:resolve(self.CustomVariable)
+  local watermark = Ast.VariableExpression(ast.globalScope, variable)
+  -- Compute the hash of the original watermark
+  local originalHash = self:hash(self.Content)
 
-  local body = ast.body;
-  local watermarkExpression = Ast.StringExpression(self.Content);
-  local scope, variable = ast.globalScope:resolve(self.CustomVariable);
-  local watermark = Ast.VariableExpression(ast.globalScope, variable);
-  local notEqualsExpression = Ast.NotEqualsExpression(watermark, watermarkExpression);
-  local ifBody = Ast.Block({Ast.ReturnStatement({})}, Scope:new(ast.body.scope));
+  -- Embed the watermark in a less obvious way
+  local embeddedWatermark = Ast.StringExpression(self.Content)
 
-  table.insert(body.statements, 1, Ast.IfStatement(notEqualsExpression, ifBody, {}, nil));
+  -- Insert the watermark check at multiple random positions
+  for i = 1, 5 do
+      local notEqualsExpression = Ast.NotEqualsExpression(watermark, embeddedWatermark)
+      local hashNotEqualsExpression = Ast.NotEqualsExpression(Ast.StringExpression(originalHash), Ast.StringExpression(self:hash(self.Content)))
+      local ifBody = Ast.Block({ Ast.ReturnStatement({}) }, Scope:new(ast.body.scope))
+      local insertPosition = math.random(1, #ast.body.statements)
+      table.insert(body.statements, insertPosition, Ast.IfStatement(Ast.OrExpression(notEqualsExpression, hashNotEqualsExpression), ifBody, {}, nil))
+  end
+
+  -- Encrypt the WatermarkCheck code
+  local encryptStringsStep = EncryptStrings:new()
+  ast = encryptStringsStep:apply(ast, pipeline)
+
+  -- Wrap the WatermarkCheck code in a function
+  local wrapInFunction = wrapInFunctionStep:new()
+  ast = wrapInFunction:apply(ast, pipeline)
+
+  return ast
 end
 
-return WatermarkCheck;
+return WatermarkCheck
